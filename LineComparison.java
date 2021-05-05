720e41cf6f4d88bbd0c20c2a0cc1e48c18a97218
ktritsh7 <ktriesh7@gmail.com>
2021-05-05 19:52:22 +0530

    Comparing the Length of Line equal or not

--- LineComparison.java
+++ LineComparison.java
@@ -6,6 +6,7 @@ public class LineComparison {
 	public static void main(String[] args) {
 		
 		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Co-Oridinates for 1st Line");
 		System.out.println("Enter X1 Value : ");
 		int X1=sc.nextInt();
 		System.out.println("Enter Y1 Value : ");
@@ -15,9 +16,24 @@ public class LineComparison {
 		System.out.println("Enter Y2 Value : ");
 		int Y2=sc.nextInt();
 		
-		double length=Math.sqrt((X2-X1)*(X2-X1)+(Y2-Y1)*(Y2-Y1));
+		double l1= Math.sqrt((X2-X1)*(X2-X1)+(Y2-Y1)*(Y2-Y1));
 		
-		System.out.println("Distance betweent the two point is : " +length);
+		System.out.println("Enter Co-Oridinates for 2nd Line");
+		System.out.println("Enter a1 Value : ");
+		int a1=sc.nextInt();
+		System.out.println("Enter b1 Value : ");
+		int b1=sc.nextInt();
+		System.out.println("Enter a2 Value : ");
+		int a2=sc.nextInt();
+		System.out.println("Enter b2 Value : ");
+		int b2=sc.nextInt();
+		
+		double l2= Math.sqrt((a2-a1)*(a2-a1)+(b2-b1)*(b2-b1));
+		
+		if (l1 == l2)
+			System.out.println("Lines are equal");
+		else
+			System.out.println("Lines are not equal");
 	}
 
 }
