434ed0090672d7cf63eaf4ff63c9f2beda646f94
ktritsh7 <ktriesh7@gmail.com>
2021-05-10 20:49:40 +0530

    Refactor: Now using Compareto method

--- LineComparison.java
+++ LineComparison.java
@@ -2,13 +2,10 @@ package com.bridgelabz.linecomparison;
 import java.util.Scanner;
 
 public class LineComparison {
-	
-	public static void comparelength() {
+
+	public static void main(String[] args) {
 		
-		//creating object of scanner class
-		Scanner sc = new Scanner(System.in);
-		
-		//taking value from users using scanner class for first line
+		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter Co-Oridinates for 1st Line");
 		System.out.println("Enter X1 Value : ");
 		int X1=sc.nextInt();
@@ -19,10 +16,8 @@ public class LineComparison {
 		System.out.println("Enter Y2 Value : ");
 		int Y2=sc.nextInt();
 		
-		//Computation
 		double l1= Math.sqrt((X2-X1)*(X2-X1)+(Y2-Y1)*(Y2-Y1));
 		
-		//taking value from users using scanner class for second line
 		System.out.println("Enter Co-Oridinates for 2nd Line");
 		System.out.println("Enter a1 Value : ");
 		int a1=sc.nextInt();
@@ -33,21 +28,20 @@ public class LineComparison {
 		System.out.println("Enter b2 Value : ");
 		int b2=sc.nextInt();
 		
-		//Computation
 		double l2= Math.sqrt((a2-a1)*(a2-a1)+(b2-b1)*(b2-b1));
 		
 		Double line1 = new Double(l1);
 		Double line2 = new Double(l2);
-		boolean equal = line1.equals(line2);
-		if (equal)
-			System.out.println("Lines are Equal");
-		else
-			System.out.println("Lines are Not Equal");
-	}
-
-	public static void main(String[] args) {
-		comparelength();
-
+		boolean ifEqual = line1.equals(line2);
+		if (ifEqual)
+			System.out.println("Both Lines are Equal");
+		else {
+			int compare = line1.compareTo(line2);
+			if (compare > 0)
+				System.out.println("Line 1 is larger");
+			else
+				System.out.println("Line 2 is larger");
+		}
 	}
 
 }
